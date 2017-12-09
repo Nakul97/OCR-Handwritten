795ab237a5f1e3a1ecfc9cbda67491f4daa27683
Nakul97 <nakul.sakhuja@gmail.com>
2017-12-10 02:31:11 +0530

    Initial Commit

---
+++ ScanThat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScanThat
+//
+//  Created by Nakul Sakhuja on 10/12/17.
+//  Copyright © 2017 camera.scan.plus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
